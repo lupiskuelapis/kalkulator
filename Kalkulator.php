6104e550c461cf3551ad5775d6a2806ffa2b7d53
lupiskuelapis <74519709+lupiskuelapis@users.noreply.github.com>
2020-12-08 22:02:05 +0700

    Add files via upload

---
+++ Kalkulator.php
@@ -0,0 +1,41 @@
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta https-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Remidi</title>
+	</head>
+	<body>
+		<form action="" method="post">
+		<table border="1">
+			<tr>
+				<td colspan="2" align="center">Form kalkulator</td>
+				</tr>
+			<tr>
+				<td>Angka 1</td>
+				<td><input type="text" name="angka1"></td>
+				</tr>
+			<tr>
+				<td>Angka 2</td>
+				<td><input type="text" name="angka2"></td>
+				</tr>
+			<tr>
+				<td>Operator</td>
+				<td>
+					<select name ="operator">
+						<option value="*">*</option>
+						<option value="/">/</option>
+						<option value="+">+</option>
+						<option value="-">-</option>
+						</select>
+					</td>
+			</tr>
+			<tr>
+				<td>
+					<input type="submit" name="hitung" value="hitung">
+					<button type="reset">reset</button>
+				</td>
+			</tr>
+			<h1><p style="text-center"> Hafidz Aulia Wicaksana</p></h1>
+			</table>
+			</form>
+			</body>
